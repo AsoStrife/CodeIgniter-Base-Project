4e12cd415699f232acdc881d6fafe1024a9e8da5
ivanmara <ivan.mara@gmail.com>
2016-05-04 15:10:15 +0200

    Add bootstrap to deactivate user

--- application/views/auth/deactivate_user.php
+++ application/views/auth/deactivate_user.php
@@ -1,18 +1,46 @@
-<h1><?php echo lang('deactivate_heading');?></h1>
-<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php $this->load->view('inc/header'); ?>
 
-<?php echo form_open("auth/deactivate/".$user->id);?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
 
-  <p>
-  	<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
-    <input type="radio" name="confirm" value="yes" checked="checked" />
-    <?php echo lang('deactivate_confirm_n_label', 'confirm');?>
-    <input type="radio" name="confirm" value="no" />
-  </p>
+		<?php echo form_open("auth/deactivate/".$user->id);?>
+		<div class="form-group">
+			<h1><?php echo lang('deactivate_heading');?></h1>
+			<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+		</div>
+		<?/*
+		<p>
+			<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
+			<input type="radio" name="confirm" value="yes" checked="checked" />
+			<?php echo lang('deactivate_confirm_n_label', 'confirm');?>
+			<input type="radio" name="confirm" value="no" />
+		</p>
+		*/?>
 
-  <?php echo form_hidden($csrf); ?>
-  <?php echo form_hidden(array('id'=>$user->id)); ?>
+		<div class="radio">
+		  	<label>	    
+		    	<input type="radio" name="confirm" value="yes" checked>
+  				<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
+		  	</label>
+		</div>
+		<div class="radio">
+		  	<label>	    
+		    	<input type="radio" name="confirm" value="no">
+  				<?php echo lang('deactivate_confirm_n_label', 'confirm');?>
+		  	</label>
+		</div>
 
-  <p><?php echo form_submit('submit', lang('deactivate_submit_btn'));?></p>
+		<?php echo form_hidden($csrf); ?>
+		<?php echo form_hidden(array('id'=>$user->id)); ?>
 
-<?php echo form_close();?>
+		<div class="form-group">
+			<?php echo form_submit('submit', lang('deactivate_submit_btn'), array("class" => 'btn btn-default'));?>
+		</div>
+		
+		<?php echo form_close();?>
+
+		</div> <!-- ./ col-lg-4 -->
+	</div> <!-- ./ row -->
+</div> <!-- ./ container -->
