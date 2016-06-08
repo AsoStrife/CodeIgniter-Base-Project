62113b8c2945b40a077273b3a4fe063b91c4abbe
Andrea <me@andreacorriga.com>
2016-06-08 11:26:31 +0200

    Dashboard bugfix

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -71,7 +71,7 @@
 	                                </div>
 	                            </div>
 	                        </div>
-	                        <a href="/admin/imageUploader/index">
+	                        <a href="/admin/images/show_images">
 	                            <div class="panel-footer">
 	                                <span class="pull-left">Vedi dettagli</span>
 	                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
